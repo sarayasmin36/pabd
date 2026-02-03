fe473ed36b70299e5d1cda4dba03920d90cc78d6
Sara <sara.y@escolar.ifrn.edu.br>
2026-02-03 20:41:09 -0300

    aaaaaaaa

--- empresa/routes/fornecedores.js
+++ empresa/routes/fornecedores.js
@@ -25,4 +25,57 @@ router.post('/salvar', (req, res) => {
     });
 });
 
+router.get('/editar/:id', (req, res) => {
+    const id = req.params.id;
+
+    conexao.query(
+        'SELECT * FROM fornecedor WHERE id_fornecedor = ?',
+        [id],
+        (err, resultado) => {
+            if (err) throw err;
+
+            if (resultado.length === 0) {
+                return res.send('Fornecedor não encontrado');
+            }
+
+            res.render('fornecedores/editar', {
+                fornecedor: resultado[0]
+            });
+        }
+    );
+});
+
+router.post('/editar/:id', (req, res) => {
+    const id = req.params.id;
+    const { nome_fornecedor, telefone, email, cnpj } = req.body;
+
+    const sql = `
+        UPDATE fornecedor
+        SET nome_fornecedor=?, telefone=?, email=?, cnpj=?
+        WHERE id_fornecedor=?
+    `;
+
+    conexao.query(
+        sql,
+        [nome_fornecedor, telefone, email, cnpj, id],
+        err => {
+            if (err) throw err;
+            res.redirect('/fornecedores');
+        }
+    );
+});
+
+router.get('/excluir/:id', (req, res) => {
+    const id = req.params.id;
+
+    conexao.query(
+        'DELETE FROM fornecedor WHERE id_fornecedor = ?',
+        [id],
+        err => {
+            if (err) throw err;
+            res.redirect('/fornecedores');
+        }
+    );
+});
+
 module.exports = router;
